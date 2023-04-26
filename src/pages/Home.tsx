4ad025c24c074465725769255a0b2668dad9d75e
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-26 11:23:07 -0300

    refactor:  Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-    const data = {
-      id: String(new Date().getTime()),
-      name: newTaskTitle,
+    const newTask = {
+      id: (new Date().getTime()),
+      title: newTaskTitle,
+      done: false,
     };
 
-    setTasks((oldState) => oldState);
+    setTasks((oldTask) => [...oldTask, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
@@ -32,18 +32,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
