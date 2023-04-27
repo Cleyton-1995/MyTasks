736b251500bf053131904cda6f2b4bcd6242aedb
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-27 18:21:35 -0300

    refactor: themes

--- src/themes/default.tsx
+++ src/themes/default.tsx
@@ -1,10 +1,17 @@
 export const ThemesFontes = {
-     bold: "Inter-Bold",
-     default: "Inter-Regular",
-     normal: "Inter-Regaulr",
-     "400": "Inter-Regular",
-     "300": "Inter-Thin",
-     "500": "Inter-Medium",
-     "600": "Inter-Medium",
-     "700": "Inter-SemiBold",
+  customFonts: {
+    inter: {
+      bold: "Inter-Bold",
+      default: "Inter-Regular",
+      normal: "Inter-Regaulr",
+      "400": "Inter-Regular",
+      "300": "Inter-Thin",
+      "500": "Inter-Medium",
+      "600": "Inter-Medium",
+      "700": "Inter-SemiBold",
+    },
+  },
+  fonts: {
+    root: "inter-bold",
+  },
    }
